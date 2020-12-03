d0ad141a502c0fe28118087928dd5146361d5542
Simon Steele <simon.steele@mymail.champlain.edu>
2020-12-03 17:08:00 -0500

    Added infinite chunk gen in GDscript

---
+++ final_project_demo/main.gd
@@ -0,0 +1,101 @@
+extends Node
+
+const ChunkResource = preload("res://Floor.tscn")
+const LengthOfSquare = 10
+var CurrentLocationOfChunk = Vector2(0,0)
+var Player
+var Blocks
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Blocks = []
+	Player = get_node("player")
+	makeNewChunk(CurrentLocationOfChunk)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var AdjustedLocation = false
+	var ShortenedPlayerPos = Vector2(Player.transform.origin.x,Player.transform.origin.z)
+	var DirVector = ShortenedPlayerPos - CurrentLocationOfChunk
+	var FinalDistance = sqrt(pow(ShortenedPlayerPos.x - CurrentLocationOfChunk.x,2)  + pow(ShortenedPlayerPos.y - CurrentLocationOfChunk.y,2))
+	
+	if DirVector.x <= -LengthOfSquare:
+		CurrentLocationOfChunk.x -= LengthOfSquare * 2
+		AdjustedLocation = true
+	if DirVector.x >= LengthOfSquare:
+		CurrentLocationOfChunk.x += LengthOfSquare * 2
+		AdjustedLocation = true
+	if DirVector.y <= -LengthOfSquare:
+		CurrentLocationOfChunk.y -= LengthOfSquare * 2
+		AdjustedLocation = true
+	if DirVector.y >= LengthOfSquare:
+		CurrentLocationOfChunk.y += LengthOfSquare * 2
+		AdjustedLocation = true
+	
+	if AdjustedLocation:
+		makeNewChunk(CurrentLocationOfChunk)
+	
+	pass
+
+func makeNewChunk(newPos):
+	
+	
+	#for node in get_children():
+	#	if(node.is_instance()):
+	#		pass
+	#pass
+	
+	#Blocks.clear()
+	
+	var NewChunk = ChunkResource.instance()
+	NewChunk.transform.origin = Vector3(newPos.x,0,newPos.y)
+	Blocks.append(self.add_child(NewChunk))
+	
+	
+	var NewChunk2 = ChunkResource.instance()
+	NewChunk2.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk2.transform.origin.x += LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk2))
+	
+	var NewChunk3 = ChunkResource.instance()
+	NewChunk3.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk3.transform.origin.x -= LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk3))
+	
+	var NewChunk4 = ChunkResource.instance()
+	NewChunk4.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk4.transform.origin.z -= LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk4))
+	
+	var NewChunk5 = ChunkResource.instance()
+	NewChunk5.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk5.transform.origin.z += LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk5))
+	
+	var NewChunk6 = ChunkResource.instance()
+	NewChunk6.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk6.transform.origin.z += LengthOfSquare * 2
+	NewChunk6.transform.origin.x += LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk6))
+	
+	var NewChunk7 = ChunkResource.instance()
+	NewChunk7.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk7.transform.origin.z += LengthOfSquare * 2
+	NewChunk7.transform.origin.x -= LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk7))
+	
+	var NewChunk8 = ChunkResource.instance()
+	NewChunk8.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk8.transform.origin.z -= LengthOfSquare * 2
+	NewChunk8.transform.origin.x += LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk8))
+	
+	var NewChunk9 = ChunkResource.instance()
+	NewChunk9.transform.origin = Vector3(newPos.x,0,newPos.y)
+	NewChunk9.transform.origin.z -= LengthOfSquare * 2
+	NewChunk9.transform.origin.x -= LengthOfSquare * 2
+	Blocks.append(self.add_child(NewChunk9))
+	
+	pass
