d885bd49990fac133db5ad362a84dff257a9123c
Chun Tao Lin <chuntao.lin@mymail.champlain.edu>
2020-12-06 17:44:11 -0500

    Using the resource that has been provided. I watched pretty much the entirety of the video and wrote out what i believe is the mesh generation part using Jakes getImage for the heightmap of perlin

--- godot-3.2.2-stable/modules/terrain/chunk_manager.h
+++ godot-3.2.2-stable/modules/terrain/chunk_manager.h
@@ -11,10 +11,13 @@
 #include "core/image.h"
 #include "core/engine.h"
 #include "chunk.h"
+#include "noise.h"
 #include "core/resource.h"
 
 class ChunkManager : public Node {
 	GDCLASS(ChunkManager, Node);
+
+
 
 	protected:
 		static void _bind_methods();
@@ -35,18 +38,39 @@ class ChunkManager : public Node {
 
 		Ref<Resource> newSource;
 
+		NoiseGenerator noiseGenerator;
+
 	public:
 		ChunkManager();
+
+		class MeshData {
+		
+
+		public:
+			PoolVector3Array *vertices;
+			PoolIntArray *triangles;
+			PoolVector2Array *uvs;
+			Array mesh_array;
+			int triangleiD;
+			MeshData(int meshX, int meshY) {
+				vertices = new PoolVector3Array[meshX * meshY];
+				triangles = new PoolIntArray[(meshX - 1) * (meshY - 1) * 6];
+				uvs = new PoolVector2Array[meshX * meshY];
+
+			}
+			void addTriangle(int a, int b, int c);
+			Ref<ArrayMesh> createMesh();
+		};
 
 	//	void _process(float delta);
 		void _update();
 		void _ready();
-		void createChunk(int desiredChunks);
+		void createChunk(Vector3 playerPos,int desiredChunks);
 		bool checkIfChunksNeedToBeReloaded(Vector2 playerPos);
 		Vector2 getCentralChunkLocation();
 		void createCube(MeshInstance* meshInstance,int x, int y, int z);
 		void makeFace(Vector3 a, Vector3 b, Vector3 c, Vector3 d);
-		
+		Ref<ArrayMesh>generateTerrainMesh(Image heightMap);
 		int numberOfChunks = 1;
 };
 
