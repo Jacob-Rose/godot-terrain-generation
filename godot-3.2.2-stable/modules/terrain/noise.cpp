6f17b33d1ea44bf2370daf4aaf5775c0dbc1a3bf
Jacob Rose <jacob.rose@mymail.champlain.edu>
2020-12-08 11:15:07 -0500

    noise improvements

--- godot-3.2.2-stable/modules/terrain/noise.cpp
+++ godot-3.2.2-stable/modules/terrain/noise.cpp
@@ -7,43 +7,78 @@
 
 void NoiseGenerator::_bind_methods() {
 	//ClassDB::bind_method(D_METHOD("createImage"), &NoiseGenerator::createImage);
-	ClassDB::bind_method(D_METHOD("getImage", "imageSize", "imageOffset", "scale"), &NoiseGenerator::getImage);
+	//ClassDB::bind_method(D_METHOD("getImage", "imageSize", "imageOffset", "scale", "octaves", "persistance", "lacunarity"), &NoiseGenerator::getImage);
 }
 
 NoiseGenerator::NoiseGenerator() {
-	/*
-	p.resize(256);
 
-	std::iota(p.begin(), p.end(), 0);
-	int seed = 1000;
-	std::default_random_engine engine(seed);
-	std::shuffle(p.begin(), p.end(), engine);
 
-	p.insert(p.end(), p.begin(), p.end());
-	*/
 }
 
 
-
-Ref<Image> NoiseGenerator::getImage(int imageSize, Vector2 imageOffset, float scale) {
+//Based of Sebastian Lague tutorial in Unity https://www.youtube.com/watch?v=MRNFcywkUSA&t=568s
+Ref<Image> NoiseGenerator::getImage(int imageSize, Vector2 imageOffset, float scale, int octaves, float persistance, float lacunarity) {
+	
 	Ref<Image> img = memnew(Image);
 	img->create(imageSize, imageSize, false, Image::Format::FORMAT_RGBA8);
 	img->lock();
+	
 	siv::PerlinNoise noiseGen;
 	noiseGen.reseed(1234); //or any seed
+	
+	Vector2* octaveOffsets = new Vector2[octaves];
 
-	for (size_t i = 0; i < imageSize; i++) {
-		for (size_t j = 0; j < imageSize; j++) {
-			float color = noiseGen.noise2D_0_1(((((double)i) / imageSize) + imageOffset.x) * scale, (((double)j / imageSize) + imageOffset.y) * scale);
-			img->set_pixel(i, j, Color(color, color, color));
+	float *noiseMap = new float[imageSize * imageSize];
+
+	for (short i = 0; i < octaves; ++i) {
+		octaveOffsets[i].x = (randFloat01() * 20000.0f) - 10000.0f;
+		octaveOffsets[i].y = (randFloat01() * 20000.0f) - 10000.0f;
+	}
+
+	
+	float maxNoiseHeight = -100.0f; //good nuf
+	float minNoiseHeight = 100.0f; //good nuf
+
+	for (int x = 0; x < imageSize; ++x) {
+		for (int y = 0; y < imageSize; ++y) {
+			float amplitude = 1.0f;
+			float frequency = 1.0f;
+			float noiseHeight = 0.0f;
+
+			for (int k =0; k < octaves; ++k) {
+				float xPos = x / scale * frequency + octaveOffsets[k].x;
+				float yPos = y / scale * frequency + octaveOffsets[k].y;
+
+				float noiseVal = (noiseGen.noise2D_0_1(xPos, yPos) * 2.0f) - 1.0f;
+				noiseHeight += noiseVal * amplitude;
+
+				amplitude *= persistance;
+				frequency *= lacunarity;
+			}
+
+			if (noiseHeight > maxNoiseHeight) {
+				maxNoiseHeight = noiseHeight;
+			} else if (noiseHeight < minNoiseHeight) {
+				minNoiseHeight = noiseHeight;
+			}
+
+			noiseMap[(y * imageSize) + x] = noiseHeight;
 		}
 	}
-	
-	return img;
-}
 
-float NoiseGenerator::noise(float x, float y, float z) {
-	return 0.0f;
+	for (int x = 0; x < imageSize; ++x) {
+		for (int y = 0; y < imageSize; ++y) {
+			noiseMap[x + (y * imageSize)] = inverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x + (y * imageSize)]);
+			img->set_pixel(x, y, Color(noiseMap[x + (y * imageSize)], noiseMap[x + (y * imageSize)], noiseMap[x + (y * imageSize)]));
+		}
+	}
+
+	delete[] noiseMap;
+	delete[] octaveOffsets;
+
+	return img;
+	
+	return nullptr;
 }
 
 //https://cs.nyu.edu/~perlin/noise/
@@ -60,4 +95,13 @@ float NoiseGenerator::grad(int hash, float x, float y, float z) {
 	float u = h < 8 ? x : y;
 	float v = h < 4 ? y : h == 12 || h == 14 ? x : z;
 	return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+}
+//https://forums.unrealengine.com/development-discussion/c-gameplay-programming/1496259-looking-for-something-like-inverselerp
+float NoiseGenerator::inverseLerp(float x, float y, float val) {
+	return (val - x) / (y - x);
+}
+
+float NoiseGenerator::randFloat01() {
+	//return float(rand()) / (float(RAND_MAX) + 1.0);
+	return 0.0f;
 }
--- godot-3.2.2-stable/modules/terrain/noise.h
+++ godot-3.2.2-stable/modules/terrain/noise.h
@@ -13,17 +13,30 @@ class NoiseGenerator : public Reference {
 	GDCLASS(NoiseGenerator, Reference);
 	protected:
 		static void _bind_methods();
-		std::vector<int> p;
 	public:
 		NoiseGenerator();
 
-		Ref<Image> getImage(int imageSize, Vector2 imageOffset, float scale);
-
-		float noise(float x, float y, float z);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="imageSize">Size in pixels of image</param>
+		/// <param name="imageOffset">Offset in pixels of noise origin</param>
+		/// <param name="scale">The zoom factor for the perlin noise</param>
+		/// <param name="octaves">The number of octaves to include in the noise, created finer details</param>
+		/// <param name="persistance">How clear the octaves show in the noise</param>
+		/// <param name="lacunarity">How clear the octaves show in the noise</param>
+		///
+		/// <example>getImage(100,Vector2(0,0), 1.0f, 3, 0.8, 2.0)</example>
+		/// <returns>Noise Image</returns>
+		Ref<Image> getImage(int imageSize, Vector2 imageOffset, float scale, int octaves, float persistance, float lacunarity);
 
 		float fade(float t);
 		float lerp(float a, float b, float t);
 		float grad(int hash, float x, float y, float z);
+		float inverseLerp(float x, float y, float val);
+
+		//https://www.oreilly.com/library/view/c-cookbook/0596007612/ch11s07.html
+		static float randFloat01();
 };
 
 
